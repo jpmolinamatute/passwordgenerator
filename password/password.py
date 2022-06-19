5c61247c0edca7b7f640590e9b8942225d0d9872
Juan P. Molina M <jmolina@resulta.com>
2022-06-18 20:05:37 -0400

    move scripting code to a singleton class

---
+++ password/password.py
@@ -0,0 +1,82 @@
+import secrets
+import string
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Password(metaclass=SingletonMeta):
+    def __init__(self, min_patter: int = 1, size: int = 10) -> None:
+        self.all_strings = [
+            string.ascii_lowercase,
+            string.ascii_uppercase,
+            string.digits,
+            string.punctuation,
+        ]
+        if not self.check_input(min_patter, size):
+            # @TODO: improve error message with something more descriptive
+            raise Exception("invalid input")
+        self.size = size
+        self.min_patter = min_patter
+
+    def check_input(self, min_patter: int, size: int) -> bool:
+        valid = True
+        if not isinstance(min_patter, int) or min_patter < 1:
+            valid = False
+        elif not isinstance(size, int) or size < min_patter * len(self.all_strings):
+            valid = False
+        return valid
+
+    def get_min_mandatory(self) -> list[str]:
+        mandatory: list[str] = []
+        for _ in range(self.min_patter):
+            mandatory += self.all_strings
+
+        return mandatory
+
+    def get_mandatory(self) -> str:
+
+        mandatory = self.get_min_mandatory()
+
+        result = ""
+        while mandatory:
+            index = secrets.choice(range(0, len(mandatory)))
+            result += secrets.choice(mandatory[index])
+            mandatory.pop(index)
+        return result
+
+    def display(self, phrase: str) -> None:
+        patt = "*"
+        empty = " "
+        max_length = 150
+        margin = (max_length - len(phrase) - 6) / 2
+        empty_length = max_length - 6
+        print(f"{patt:*^{max_length}}")
+        print(f"{patt:*^{max_length}}")
+        print(f"{patt:*^{max_length}}")
+        print(f"{patt:*^3}{empty: ^{empty_length}}{patt:*^3}")
+        print(f"{patt:*^3}{empty: ^{empty_length}}{patt:*^3}")
+        print(f"{patt:*^3}{empty: ^{margin}}{phrase}{empty: ^{margin}}{patt:*^3}")
+        print(f"{patt:*^3}{empty: ^{empty_length}}{patt:*^3}")
+        print(f"{patt:*^3}{empty: ^{empty_length}}{patt:*^3}")
+        print(f"{patt:*^{max_length}}")
+        print(f"{patt:*^{max_length}}")
+        print(f"{patt:*^{max_length}}")
+
+    def run(self) -> None:
+        whole_sample = string.ascii_lowercase
+        whole_sample += string.ascii_uppercase
+        whole_sample += string.digits
+        whole_sample += string.punctuation
+        mandatory = self.get_mandatory()
+        size_left = self.size - len(mandatory)
+        for _ in range(size_left):
+            mandatory += secrets.choice(seq=whole_sample)
+        self.display(mandatory)
--- run.py
+++ run.py
@@ -1,71 +1,16 @@
 #! /usr/bin/env python
 
 import logging
-import secrets
-import string
 import sys
 from os import path
 
-
-def get_min_mandatory(min_sample: int) -> list[str]:
-    all_strings = [
-        string.ascii_lowercase,
-        string.ascii_uppercase,
-        string.digits,
-        string.punctuation,
-    ]
-    mandatory: list[str] = []
-    for _ in range(min_sample):
-        mandatory += all_strings
-
-    return mandatory
-
-
-def get_mandatory(min_sample: int = 1) -> str:
-    if min_sample < 1:
-        raise Exception("min value is 1")
-    mandatory = get_min_mandatory(min_sample)
-
-    result = ""
-    while mandatory:
-        index = secrets.choice(range(0, len(mandatory)))
-        result += secrets.choice(mandatory[index])
-        mandatory.pop(index)
-    return result
-
-
-def display(phrase: str) -> None:
-    patt = "*"
-    empty = " "
-    max_length = 150
-    margin = (max_length - len(phrase) - 6) / 2
-    empty_length = max_length - 6
-    print(f"{patt:*^{max_length}}")
-    print(f"{patt:*^{max_length}}")
-    print(f"{patt:*^{max_length}}")
-    print(f"{patt:*^3}{empty: ^{empty_length}}{patt:*^3}")
-    print(f"{patt:*^3}{empty: ^{empty_length}}{patt:*^3}")
-    print(f"{patt:*^3}{empty: ^{margin}}{phrase}{empty: ^{margin}}{patt:*^3}")
-    print(f"{patt:*^3}{empty: ^{empty_length}}{patt:*^3}")
-    print(f"{patt:*^3}{empty: ^{empty_length}}{patt:*^3}")
-    print(f"{patt:*^{max_length}}")
-    print(f"{patt:*^{max_length}}")
-    print(f"{patt:*^{max_length}}")
+from password import Password
 
 
 def main() -> int:
-    size = 30
-    min_patter = 3
-    whole_sample = string.ascii_lowercase
-    whole_sample += string.ascii_uppercase
-    whole_sample += string.digits
-    whole_sample += string.punctuation
-    mandatory = get_mandatory(min_patter)
-    size_left = size - len(mandatory)
-    for _ in range(size_left):
-        mandatory += secrets.choice(seq=whole_sample)
-    display(mandatory)
-
+    generate = Password(2, 20)
+    generate.run()
+    logging.info("Bey!")
     return 0
 
 
@@ -75,8 +20,6 @@ if __name__ == "__main__":
     logging.info("Script %s has started", path.basename(__file__))
     try:
         EXIT_STATUS = main()
-    except KeyboardInterrupt:
-        logging.info("Bye!")
     except Exception as e:
         logging.exception(e)
         EXIT_STATUS = 2
