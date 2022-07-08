a84db84a4d7ab47ead30238ee58cbed10782b0d0
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2022-07-08 16:51:04 -0400

    added LICENSE and profiler

---
+++ profiler.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import logging
+import sys
+from os import path
+import cProfile
+import pstats
+from passwordgenerator import PasswordGenerator
+
+
+def main() -> int:
+    with cProfile.Profile() as pr:
+        generate = PasswordGenerator(2, 30)
+        generate.display()
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.print_stats(30)
+    return 0
+
+
+if __name__ == "__main__":
+    EXIT_STATUS = 1
+    logging.basicConfig(level=logging.DEBUG)
+
+    logging.info("Script %s has started", path.basename(__file__))
+    try:
+        EXIT_STATUS = main()
+        logging.info("Bye!")
+    except Exception as e:
+        logging.exception(e)
+        EXIT_STATUS = 2
+    finally:
+        sys.exit(EXIT_STATUS)
